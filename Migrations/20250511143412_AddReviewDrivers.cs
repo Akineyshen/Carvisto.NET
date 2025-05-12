43c7a59a87db4c7225f60c76ad73bc88ea0b6264
fl1ker <tutskialex@gmail.com>
2025-05-12 18:58:29 +0200

    feat: add database migrations

---
+++ Migrations/20250511143412_AddReviewDrivers.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Carvisto.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReviewDrivers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DriverReviews",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    DriverId = table.Column<string>(type: "TEXT", nullable: false),
+                    ReviewerId = table.Column<string>(type: "TEXT", nullable: false),
+                    Rating = table.Column<int>(type: "INTEGER", nullable: false),
+                    Comment = table.Column<string>(type: "TEXT", maxLength: 500, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DriverReviews", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DriverReviews_AspNetUsers_DriverId",
+                        column: x => x.DriverId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_DriverReviews_AspNetUsers_ReviewerId",
+                        column: x => x.ReviewerId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DriverReviews_DriverId",
+                table: "DriverReviews",
+                column: "DriverId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DriverReviews_ReviewerId",
+                table: "DriverReviews",
+                column: "ReviewerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DriverReviews");
+        }
+    }
+}
